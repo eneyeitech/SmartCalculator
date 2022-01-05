0aefd459b4b1ed6a1b1e7328df1e86531eaa271e
Abdulmumin Eneye Abdulkarim <abdulmumineabdulkarim@gmail.com>
2022-01-05 19:09:31 +0100

    Complete 2

--- src/com/eneyeitech/Main.java
+++ src/com/eneyeitech/Main.java
@@ -7,13 +7,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        int number1 = scanner.nextInt();
-        int number2 = scanner.nextInt();
 
-        System.out.println(sum(number1, number2));
+        while (true) {
+            String line = scanner.nextLine();
+            String[] values = line.split(" ");
+
+            if ("/exit".equals(line)) {
+                System.out.println("Bye!");
+                break;
+            }
+            if ("".equals(line)) {
+
+            } else {
+                if (values.length >= 2) {
+                    calculateSum(values);
+                } else if (values.length == 1) {
+                    System.out.println(values[0]);
+                }
+            }
+        }
     }
 
     public static int sum(int a, int b) {
         return a + b;
     }
+
+    public static void calculateSum(String[] values) {
+        int a = 0;
+        int b = 0;
+        try {
+            a = Integer.parseInt(values[0]);
+            b = Integer.parseInt(values[1]);
+            System.out.println(sum(a, b));
+
+        } catch (NumberFormatException e) {
+
+        }
+    }
 }
+
